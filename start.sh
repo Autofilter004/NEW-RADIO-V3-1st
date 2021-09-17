2ac93621cfb51a158dc627085c546d2087c83f5c
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-09-17 10:39:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/AsmSafone/RadioPlayerV3.git /RadioPlayerV3
-cd /RadioPlayerV3
+git clone https://github.com/Autofilter004/NEW-RADIO-V3-1st.git /NEW-RADIO-V3-1st
+cd /NEW-RADIO-V3-1st
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 main.py
