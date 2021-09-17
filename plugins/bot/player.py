608234775050878c4e1f968cd9e72be5fb64b56d
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-09-17 11:49:05 +0530

    Update player.py

--- plugins/bot/player.py
+++ plugins/bot/player.py
@@ -569,13 +569,13 @@ allcmd = ["play", "current", "playlist", "song", f"song@{USERNAME}", f"play@{USE
 @Client.on_message(filters.command(allcmd) & filters.group & ~filters.chat(CHAT) & ~filters.chat(LOG_GROUP))
 async def not_chat(_, m: Message):
     buttons = [
+
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/SafoTheBot"),
-            ],
-            [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/RadioPlayerV3"),
+
+                InlineKeyboardButton("🔷 CONTACT  ONWER 🔷", url="https://t.me/mhd_thanzeer"),
+
             ]
+
          ]
     k=await m.reply_photo(photo="https://telegra.ph/file/4e839766d45935998e9c6.jpg", caption="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/AsmSafone/RadioPlayerV3) Below 😉!**", reply_markup=InlineKeyboardMarkup(buttons))
     await mp.delete(m)
